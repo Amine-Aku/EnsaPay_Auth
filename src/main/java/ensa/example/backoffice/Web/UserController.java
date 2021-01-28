dad0765d9c20438b335008310d68f9964c8c4bd4
Amine-Aku <codenameaku@gmail.com>
2021-01-28 23:08:07 +0100

    current Agent: test deploy2: added dependencies (import)

--- src/main/java/ensa/example/backoffice/Web/UserController.java
+++ src/main/java/ensa/example/backoffice/Web/UserController.java
@@ -2,11 +2,15 @@ package ensa.example.backoffice.Web;
 
 
 
+import ensa.example.backoffice.Entities.UserApp;
+import ensa.example.backoffice.Repository.UserRepository;
 import ensa.example.backoffice.Service.ResponseMessage;
 import ensa.example.backoffice.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.multipart.MultipartFile;
