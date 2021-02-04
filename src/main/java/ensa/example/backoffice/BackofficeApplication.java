e95dd22ed0ecaed1e924de6e170f72c59852502f
Amine-Aku <codenameaku@gmail.com>
2021-02-04 18:43:22 +0100

    add @Bean resttemplate

--- src/main/java/ensa/example/backoffice/BackofficeApplication.java
+++ src/main/java/ensa/example/backoffice/BackofficeApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 
