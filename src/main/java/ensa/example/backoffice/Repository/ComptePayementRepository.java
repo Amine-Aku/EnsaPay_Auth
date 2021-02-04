dffa468e1354b4021f32de816c9707e318bde4ae
Amine-Aku <codenameaku@gmail.com>
2021-02-04 22:18:21 +0100

    Clone Revert

--- src/main/java/ensa/example/backoffice/Repository/ComptePayementRepository.java
+++
@@ -1,8 +0,0 @@
-package ensa.example.backoffice.Repository;
-
-
-import ensa.example.backoffice.Entities.ComptePayement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComptePayementRepository extends JpaRepository<ComptePayement,Integer> {
-}
