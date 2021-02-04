1b9a75de7fd59cff0dfad4e690274db206cd2d0c
Amine-Aku <codenameaku@gmail.com>
2021-02-04 20:55:12 +0100

    merge MS-bank with MS-backoffice

--- src/main/java/ensa/example/backoffice/Config/TaskletStep.java
+++ src/main/java/ensa/example/backoffice/Config/TaskletStep.java
@@ -37,7 +37,7 @@ public class TaskletStep implements Tasklet {
 
                 System.out.println("### TaskletStep:" + file.getName() + " is deleted!");
 
-                this.setCsvFilename(csvFilename"-"+i+".csv");
+                this.setCsvFilename(csvFilename+"-"+i+".csv");
                 File file2 = new File(this.getCsvFilename());
                 file2.createNewFile();
 
